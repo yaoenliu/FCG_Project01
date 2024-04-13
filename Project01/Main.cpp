e0942e85239981c96a149a784574df418f574401
yaoenliu <peterliu1060@gmail.com>
2024-04-13 15:42:51 +0800

    Added perspective movement function

--- Project01/Main.cpp
+++ Project01/Main.cpp
@@ -14,12 +14,21 @@
 // Function prototypes here
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
 
 // camera and color variables
 glm::mat4 proj;
 glm::mat4 view;
 glm::vec4 color;
-float angle = 90.0f;
+glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+float horizontal_angle = 90;
+float vertical_angle = 0;
+
+bool keyS[4] = { false, false, false, false };
+bool mouseS[2] = { false, false };
+void cameaMove();
 
 // global shader info
 ShaderInfo shaders[] = {
@@ -39,7 +48,7 @@ int main()
 	GLFWwindow* window = glfwCreateWindow(1920, 1080, "Hello World", NULL, NULL);
 	// set up the camera
 	proj = glm::perspective(glm::radians(45.0f), 640.0f / 480.0f, 0.1f, 100.0f);
-	view = glm::lookAt(glm::vec3(0.0f, 2.0f, 3.0f), // camera position
+	view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f), // camera position
 		glm::vec3(0.0f, 0.0f, 0.0f), // target position
 		glm::vec3(0.0f, 1.0f, 0.0f)); // up vector
 
@@ -59,6 +68,8 @@ int main()
 	// Set callback function
 	glfwSetKeyCallback(window, key_callback);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetMouseButtonCallback(window, mouse_button_callback);
 
 	// Make the window's context current
 	glfwMakeContextCurrent(window);
@@ -78,6 +89,8 @@ int main()
 	obj.pushVertex(glm::vec3(-0.5f, -0.5f, 0.0f));
 	obj.pushVertex(glm::vec3(0.5f, -0.5f, 0.0f));
 	obj.pushVertex(glm::vec3(0.0f, 0.5f, 0.0f));
+	obj.setPosition(position);
+
 
 	// setup imgui
 	IMGUI_CHECKVERSION();
@@ -89,11 +102,11 @@ int main()
 	// Setup Platform/Renderer bindings
 	ImGui_ImplOpenGL3_Init("#version 330");
 
-
-
 	// Loop until the user closes the window
 	while (!glfwWindowShouldClose(window))
 	{
+
+		cameaMove();
 
 		// Render here
 		glClear(GL_COLOR_BUFFER_BIT);
@@ -113,16 +126,17 @@ int main()
 
 		ImGui::Begin("Hello, world!");
 		ImGui::SetWindowSize(ImVec2(480, 270));
-		ImGui::Text("This is some useful text.");
-		// a slider for changing the rotation angle
-		ImGui::SliderFloat("angle", &angle, -179.0f, 179.0f);
-		if (angle == 0.0f) // make sure the triangle is always visible
-			angle = 0.1f;
-		view = glm::lookAt(glm::vec3(3 * cos(glm::radians(angle)), 2.0f, 3 * sin(glm::radians(angle))), // camera position
-			glm::vec3(0.0f, 0.0f, 0.0f), // target position
-			glm::vec3(0.0f, 1.0f, 0.0f)); // up vector
-		ImGui::ColorEdit3("color", &color[0]);
 
+
+		ImGui::ColorEdit3("color", &color[0]);
+		ImGui::SliderFloat("posx", &position.x, -10.0f, 10.0f);
+		ImGui::SliderFloat("posy", &position.y, -10.0f, 10.0f);
+		ImGui::SliderFloat("posz", &position.z, -10.0f, 10.0f);
+		if (ImGui::Button("reset"))
+		{
+			position = glm::vec3(0.0f, 0.0f, 0.0f);
+		}
+		obj.setPosition(position);
 		// Random Color Button
 		if (ImGui::Button("Random Color"))
 		{
@@ -154,6 +168,14 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 {
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	if (key == GLFW_KEY_W)
+		keyS[0] = action == GLFW_RELEASE ? false : true;
+	if (key == GLFW_KEY_A)
+		keyS[1] = action == GLFW_RELEASE ? false : true;
+	if (key == GLFW_KEY_S)
+		keyS[2] = action == GLFW_RELEASE ? false : true;
+	if (key == GLFW_KEY_D)
+		keyS[3] = action == GLFW_RELEASE ? false : true;
 }
 
 // resize callback function
@@ -164,4 +186,53 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 	proj = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
 }
 
+void cameaMove()
+{
+	if (keyS[0])
+		cameraPos.z -= 0.1f;
+	if (keyS[1])
+		cameraPos.x -= 0.1f;
+	if (keyS[2])
+		cameraPos.z += 0.1f;
+	if (keyS[3])
+		cameraPos.x += 0.1f;
+	view = glm::lookAt(cameraPos, // camera position
+		glm::vec3(0.0f, 0.0f, 0.0f), // target position
+		glm::vec3(0.0f, 1.0f, 0.0f)); // up vector
+}
 
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	static double lastX = xpos;
+	static double lastY = ypos;
+	if (!mouseS[0])
+	{
+		lastX = xpos;
+		lastY = ypos;
+		glfwSetWindowTitle(window, ("(" + std::to_string(xpos) + ", " + std::to_string(ypos) + ") angle ("+ std::to_string(horizontal_angle) + ", " + std::to_string(vertical_angle) + ")").c_str());
+		return;
+	}	
+
+	horizontal_angle += 0.01f * (lastX - xpos);
+	vertical_angle -= 0.01f * (lastY - ypos);
+	if (horizontal_angle > 360)horizontal_angle -= 360;
+	if (horizontal_angle < 0)horizontal_angle += 360;
+	if (vertical_angle > 90)vertical_angle = 90;
+	if (vertical_angle < -90)vertical_angle = -90;
+	cameraPos = glm::vec3(3*cos(vertical_angle) * sin(horizontal_angle), 3 * sin(vertical_angle), 3 * cos(vertical_angle) * cos(horizontal_angle));
+	view = glm::lookAt(cameraPos, // camera position
+		glm::vec3(0.0f, 0.0f, 0.0f), // target position
+		glm::vec3(0.0f, 1.0f, 0.0f)); // up vector
+	// reset the last position
+
+	lastX = xpos;
+	lastY = ypos;
+}
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+	if (button == GLFW_MOUSE_BUTTON_LEFT)
+		mouseS[0] = action == GLFW_RELEASE ? false : true;
+	if (button == GLFW_MOUSE_BUTTON_RIGHT)
+		mouseS[1] = action == GLFW_RELEASE ? false : true;
+}
--- Project01/Object.h
+++ Project01/Object.h
@@ -11,11 +11,14 @@ private:
 	GLID ID;
 
 	std::vector<glm::vec3> vertices;
-	glm::vec3 translation;
+	glm::vec3 position;
+	glm::mat4 translation;
 	glm::fquat rotation;
 	glm::mat4 mRotation;
 	glm::vec3 scale;
-	
+
+	glm::mat4 ObjectMatrix;
+
 
 public:
 	Object();
@@ -24,15 +27,22 @@ public:
 	void AddChild(Object* child);
 	void pushVertex(glm::vec3 vertex);
 	void Init(GLuint shaderProgram);
+
+	void setPosition(glm::vec3 newPose);
+	void setRotation(glm::fquat rotation);
+	void setScale(glm::vec3 scale);
+
 	std::vector<Object*> childrens;
 };
 
 Object::Object()
 {
-	translation = glm::vec3(0.0f, 0.0f, 0.0f);
+	position = glm::vec3(0.0f, 0.0f, 0.0f);
+	translation = glm::mat4(1.0f);
 	rotation = glm::fquat(1.0f, 0.0f, 0.0f, 0.0f);
 	scale = glm::vec3(1.0f, 1.0f, 1.0f);
 	mRotation = glm::mat4(1.0f);
+	ObjectMatrix = glm::mat4(1.0f);
 }
 
 Object::~Object()
@@ -63,7 +73,20 @@ void Object::Draw()
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
+	ObjectMatrix = translation * mRotation * glm::scale(glm::mat4(1.0f), scale);
+
 	GLint rotationLoc = glGetUniformLocation(ID.ShaderProgram, "rotation");
-	glUniformMatrix4fv(rotationLoc, 1, GL_FALSE, &mRotation[0][0]);
+	glUniformMatrix4fv(rotationLoc, 1, GL_FALSE, &ObjectMatrix[0][0]);
 	glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+}
+
+void Object::setPosition(glm::vec3 newPose)
+{
+	this->position = newPose;
+	this->translation = glm::translate(glm::mat4(1.0f), newPose);
+	glm::vec4 t = glm::vec4(newPose, 1);
+	glm::vec4 c1 = glm::vec4(1, 0, 0, 0);
+	glm::vec4 c2 = glm::vec4(0, 1, 0, 0);
+	glm::vec4 c3 = glm::vec4(0, 0, 1, 0);
+	translation = glm::mat4(c1, c2, c3, t);
 }
