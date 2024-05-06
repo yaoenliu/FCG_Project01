cad604507ba120de8882c57b430b81592b397bd8
Toby1009 <st99005912@gmail.com>
2024-05-06 22:40:32 +0800

    add model refraction

--- Project01/shader/RobotFragmentShader.glsl
+++ Project01/shader/RobotFragmentShader.glsl
@@ -60,6 +60,14 @@ void main()
         FragColor = vec4(texture(skybox, R).rgb, 1.0);
         return;
     }
+    else if(mayType == 3)
+    {
+        float ratio = 1.00 / 1.52;
+        vec3 I = normalize(FragPos - viewPos);
+        vec3 R = refract(I, normalize(Normal), ratio);
+        FragColor = vec4(texture(skybox, R).rgb, 1.0);
+        return;
+    }
 
     if(!hasTexture) 
     {
